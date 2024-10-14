324c27f45e9acbf8d1077161621ccc92cd47adcb
TuanMudaIrfan <133212382+TuanMudaIrfan@users.noreply.github.com>
2024-10-14 22:25:16 +0700

    Add files via upload

---
+++ pesanan/pesanan/js/script.js
@@ -0,0 +1,28 @@
+// Ambil elemen
+const nameInputPage = document.getElementById('name-input-page');
+const birthdayMessagePage = document.getElementById('birthday-message-page');
+const nameInput = document.getElementById('nameInput');
+const nameDisplay = document.getElementById('nameDisplay');
+const submitBtn = document.getElementById('submitBtn');
+const backBtn = document.getElementById('backBtn');
+
+// Fungsi untuk menampilkan ucapan ulang tahun
+submitBtn.addEventListener('click', function() {
+    const name = nameInput.value.trim().toLowerCase();
+    if (name.includes("rifqa")) {
+        // Jika nama mengandung "rifqa", tampilkan ucapan ulang tahun
+        nameDisplay.textContent = nameInput.value.trim();
+        nameInputPage.classList.add('hidden');
+        birthdayMessagePage.classList.remove('hidden');
+    } else {
+        // Jika tidak mengandung "rifqa", tampilkan pesan peringatan
+        alert('Bukan woi');
+    }
+});
+
+// Fungsi untuk kembali ke halaman input nama
+backBtn.addEventListener('click', function() {
+    nameInputPage.classList.remove('hidden');
+    birthdayMessagePage.classList.add('hidden');
+    nameInput.value = '';
+});
